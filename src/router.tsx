858642725dd726aed16ba6da4295716cf8873fd8
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-01 22:06:44 -0500

    feat: add AuthLayout and LoginView components for authentication routing

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AppLayout from "./layouts/AppLayout"
 import DashboardView from "./views/DashboardView"
+import AuthLayout from "./layouts/AuthLayout"
+import LoginView from "./views/auth/LoginView"
 
 export default function Router() {
 
@@ -8,7 +10,11 @@ export default function Router() {
         <BrowserRouter>
             <Routes>
                 <Route element={<AppLayout />}>
-                    <Route path="/" element={<DashboardView />} index />
+                    <Route path="" element={<DashboardView />} />
+                </Route>
+
+                <Route element={<AuthLayout />}>
+                    <Route path="/" element={<LoginView />} index />
                 </Route>
             </Routes>
         </BrowserRouter>
