1c0dd7573870b35f763c961ad730278d95ddb29c
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-04 22:10:33 -0500

    feat: add Logo component to display the GymDashboard logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export default function Logo() {
+    return (
+        <img src="/logo.svg" alt="Logotipo GymDashboard" width="150" height="50" className="pt-10 lg:pt-20 pb-5 lg:pb-10" />
+    )
+}
