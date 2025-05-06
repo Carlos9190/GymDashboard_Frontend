6bf976fb4fff625df86f3723f1efbbab6b71843f
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-05 21:23:50 -0500

    feat: add navigation link to registration page in LoginView

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { UserLoginForm } from "../../types";
 import ErrorMessage from "../../components/ErrorMessage";
+import { Link } from "react-router-dom";
 
 export default function LoginView() {
 
@@ -66,6 +67,13 @@ export default function LoginView() {
           className="bg-red-600 hover:bg-red-700 w-full p-3 text-white font-black text-xl cursor-pointer rounded-lg"
         />
       </form>
+
+      <nav className="mt-10 flex flex-col space-y-4">
+          <Link
+            to={'/auth/register'}
+            className="text-center text-gray-300 font-normal"
+          >Do not have account yet? Create one</Link>
+      </nav>
     </>
   )
 }
