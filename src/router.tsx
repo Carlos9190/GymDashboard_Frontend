7178df4dabb07e9cbecaa5078ef387eff04f9f3d
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-06 17:38:44 -0500

    feat: add account confirmation feature

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AuthLayout from "./layouts/AuthLayout"
 import LoginView from "./views/auth/LoginView"
 import RegisterView from "./views/auth/RegisterView"
+import ConfirmAccountView from "./views/auth/ConfirmAccountView"
 
 export default function Router() {
 
@@ -11,6 +12,7 @@ export default function Router() {
                 <Route element={<AuthLayout />}>
                     <Route path="/auth/login" element={<LoginView />} />
                     <Route path="/auth/register" element={<RegisterView />} />
+                    <Route path="/auth/confirm-account" element={<ConfirmAccountView />} />
                 </Route>
             </Routes>
         </BrowserRouter>
