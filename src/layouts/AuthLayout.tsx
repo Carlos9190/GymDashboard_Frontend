9236ecc8e680e7ea3f9123bcfcf7f657512003bb
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-05 21:23:28 -0500

    feat: integrate ToastContainer into AuthLayout for notification handling

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom"
 import Logo from "../components/Logo"
+import { ToastContainer } from "react-toastify"
 
 export default function AuthLayout() {
   return (
     <>
       <div className="bg-gray-900 min-h-screen flex flex-col items-center">
         <Logo />
-        <div className="mx-auto w-[450px]">
+        <div className="mx-auto w-[450px] pb-10">
           <Outlet />
         </div>
       </div>
+
+      <ToastContainer
+        pauseOnHover={false}
+        pauseOnFocusLoss={false}
+      />
     </>
   )
 }
