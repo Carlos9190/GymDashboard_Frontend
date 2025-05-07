1fb8c1888bfdb4038cae86c6dfa7b32fb57ba0a6
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-07 16:38:42 -0500

    feat: implement password reset functionality with forgot password and new password views

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "../lib/axios"
 import { isAxiosError } from "axios"
-import { ApiResponse, ConfirmToken, RequestConfirmationTokenForm, UserLoginForm, UserRegistrationForm } from "../types"
+import { ApiResponse, ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationTokenForm, UserLoginForm, UserRegistrationForm } from "../types"
 
 export async function createAccount(formData: UserRegistrationForm) {
     try {
@@ -42,6 +42,43 @@ export async function login(formData: UserLoginForm) {
     try {
         const url = '/auth/login'
         const { data } = await api.post<ApiResponse>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.message)
+        }
+    }
+}
+
+export async function forgotPassword(formData: ForgotPasswordForm) {
+    try {
+        const url = '/auth/forgot-password'
+        const { data } = await api.post<ApiResponse>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.message)
+        }
+    }
+}
+
+export async function validateToken(formData: ConfirmToken) {
+    try {
+        const url = '/auth/validate-token'
+        const { data } = await api.post<ApiResponse>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.message)
+        }
+    }
+}
+
+export async function updatePasswordWithToken({ formData, token }: { formData: NewPasswordForm, token: ConfirmToken['token'] }) {
+    try {
+        const url = `/auth/update-password/${token}`
+        const { data } = await api.post<ApiResponse>(url, formData)
+        console.log(data)
         return data
     } catch (error) {
         if (isAxiosError(error) && error.response) {
--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,8 @@ import LoginView from "./views/auth/LoginView"
 import RegisterView from "./views/auth/RegisterView"
 import ConfirmAccountView from "./views/auth/ConfirmAccountView"
 import RequestNewTokenView from "./views/auth/RequestNewTokenView"
+import ForgotPasswordView from "./views/auth/ForgotPasswordView"
+import NewPasswordView from "./views/auth/NewPasswordView"
 
 export default function Router() {
 
@@ -15,6 +17,8 @@ export default function Router() {
                     <Route path="/auth/register" element={<RegisterView />} />
                     <Route path="/auth/confirm-account" element={<ConfirmAccountView />} />
                     <Route path="/auth/request-token" element={<RequestNewTokenView />} />
+                    <Route path="/auth/forgot-password" element={<ForgotPasswordView />} />
+                    <Route path="/auth/new-password" element={<NewPasswordView />} />
                 </Route>
             </Routes>
         </BrowserRouter>
