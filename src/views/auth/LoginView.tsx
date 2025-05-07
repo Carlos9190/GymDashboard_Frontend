c1bca6ffcd9b53f9b51173a1e0df30d8972843dd
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-07 18:01:11 -0500

    feat: add navigation to home on successful login

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { UserLoginForm } from "../../types";
 import ErrorMessage from "../../components/ErrorMessage";
@@ -14,6 +14,7 @@ export default function LoginView() {
   }
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
+  const navigate = useNavigate()
   const { mutate } = useMutation({
     mutationFn: login,
     onError: (error) => {
@@ -21,6 +22,7 @@ export default function LoginView() {
     },
     onSuccess: (data) => {
       toast.success(data?.message)
+      navigate('/')
     }
   })
 
